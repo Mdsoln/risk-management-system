0487fd1fc3c46aee7c46efb05a45ca547e7c0178
Mdsoln <muddyfakih98@gmail.com>
2025-06-05 10:55:05 +0300

    change url

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
         return [
             {
                 source: '/api/:path*',
-                destination: process.env.BACKEND_URL || 'http://localhost:8080/api/:path*',
+                destination: 'http://68.183.158.185/:8080/api/:path*',
             },
         ];
     },
